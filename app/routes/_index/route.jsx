b8ea93cb8e28191e1b21c6b5eb02e81340a273f5
TChukwuleta <tobechichukwuleta125@gmail.com>
2024-11-27 11:34:13 +0100

    Update default page

--- app/routes/_index/route.jsx
+++ app/routes/_index/route.jsx
@@ -19,36 +19,38 @@ export default function App() {
   return (
     <div className={styles.index}>
       <div className={styles.content}>
-        <h1 className={styles.heading}>A short heading about [your app]</h1>
+        <h1 className={styles.heading}>Accept Bitcoin in Shopify with BTCPay Server</h1>
         <p className={styles.text}>
-          A tagline about [your app] that describes your value proposition.
+          Add Bitcoin as a payment option to your store with{" "}
+          <a
+            href="https://btcpayserver.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ color: "#3498db", textDecoration: "underline" }}
+          >
+            BTCPay Server
+          </a>
+          . It's free, simple, secure, and puts you in full control.
         </p>
-        {showForm && (
-          <Form className={styles.form} method="post" action="/auth/login">
-            <label className={styles.label}>
-              <span>Shop domain</span>
-              <input className={styles.input} type="text" name="shop" />
-              <span>e.g: my-shop-domain.myshopify.com</span>
-            </label>
-            <button className={styles.button} type="submit">
-              Log in
-            </button>
-          </Form>
-        )}
+
         <ul className={styles.list}>
           <li>
-            <strong>Product feature</strong>. Some detail about your feature and
-            its benefit to your customer.
+            <strong>Zero fees: </strong> Enjoy a payment gateway with no fees. Yes, You saw that right. Zero fees!
           </li>
           <li>
-            <strong>Product feature</strong>. Some detail about your feature and
-            its benefit to your customer.
+            <strong>No middlemen: </strong> Say goodbye to intermediaries and tedious paperwork, and get your money directly in your wallet
           </li>
           <li>
-            <strong>Product feature</strong>. Some detail about your feature and
-            its benefit to your customer.
+            <strong>Community-driven support: </strong> Get responsive assistance from our dedicated community
           </li>
         </ul>
+        <div>
+          <a className={styles.button} 
+          target="_blank"
+          href="https://docs.btcpayserver.org/Shopify/">
+            Get Started
+          </a>
+        </div>
       </div>
     </div>
   );
