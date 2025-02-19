810348879b0ca71fd1aa375037a8d9be67ebbc11
nicolas.dorier <nicolas.dorier@gmail.com>
2025-02-19 17:08:24 +0900

    Add github actions

---
+++ .github/workflows/publish-docker.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+TAG="${GITHUB_REF#refs/tags/}"
+
+sudo docker buildx build \
+    -t "btcpayserver/shopify-app-deployer:${TAG}" . \
+    --platform linux/amd64,linux/arm64,linux/arm/v7 \
+    --push .
+    
