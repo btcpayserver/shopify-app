82f4037f389c4df937d74e60d6c12ed5e611befe
Chukwuleta Tobechi <47084273+TChukwuleta@users.noreply.github.com>
2025-02-19 15:42:52 -0800

    Display custom BTCPay payment for only manual/custom payment

--- extensions/btcpaycheckout/src/Checkout.jsx
+++ extensions/btcpaycheckout/src/Checkout.jsx
@@ -3,8 +3,10 @@ import {
   BlockStack,
   Button,
   Text,
-  useApi
+  useApi,
+  useSelectedPaymentOptions
 } from "@shopify/ui-extensions-react/checkout";
+import { useEffect, useState } from "react";
 
 // 1. Choose an extension target
 export default reactExtension(
@@ -13,16 +15,28 @@ export default reactExtension(
 );
 
 function Extension() {
+  const options = useSelectedPaymentOptions();
   const { shop, checkoutToken } = useApi();
+  const [hasManualPayment, setHasManualPayment] = useState(false);
+
+  
+  useEffect(() => { 
+    const hasManualPaymentOption = options.some((option) => option.type.toLowerCase() === 'manualpayment');
+    setHasManualPayment(hasManualPaymentOption);
+  }, [options]);
+
   const appUrl = `PLUGIN_URL/checkout?checkout_token=${checkoutToken.current}`;
+
+  if (!hasManualPayment) {
+    return null;
+  }
+
   return (
-    <>
-        <BlockStack>
-          <Text>Shop name: {shop.name}</Text>
-          <Text size="large" alignment="center" bold>Review and pay using BTCPay Server!</Text>
-          <Text>Please review your order and complete the payment using BTCPay Server.</Text>
-          <Button to={appUrl} external>Complete Payment</Button>
-        </BlockStack>
-    </>
+    <BlockStack>
+      <Text>Shop name: {shop.name}</Text>
+      <Text size="large" alignment="center" bold>Review and pay using BTCPay Server!</Text>
+      <Text>Please review your order and complete the payment using BTCPay Server.</Text>
+      <Button to={appUrl} external>Complete Payment</Button>
+    </BlockStack>
   );
 }
