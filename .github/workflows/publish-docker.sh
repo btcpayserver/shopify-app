bbdb18e8ca6a20bf2d6a43f7d6c51c6d3cd75d4b
nicolas.dorier <nicolas.dorier@gmail.com>
2025-02-19 17:12:21 +0900

    Fix github action

--- .github/workflows/publish-docker.sh
+++ .github/workflows/publish-docker.sh
@@ -2,8 +2,9 @@
 
 TAG="${GITHUB_REF#refs/tags/}"
 
+sudo docker buildx create --use
 sudo docker buildx build \
-    -t "btcpayserver/shopify-app-deployer:${TAG}" . \
+    -t "btcpayserver/shopify-app-deployer:${TAG}" \
     --platform linux/amd64,linux/arm64,linux/arm/v7 \
     --push .
     
