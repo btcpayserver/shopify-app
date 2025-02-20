0d4ca742d38d64b2e002a6a6757fcf75eba004e8
Chukwuleta Tobechi <47084273+TChukwuleta@users.noreply.github.com>
2025-02-20 06:22:34 -0800

    Only check for manual payment type

--- extensions/btcpaycheckout/src/Checkout.jsx
+++ extensions/btcpaycheckout/src/Checkout.jsx
@@ -6,7 +6,6 @@ import {
   useApi,
   useSelectedPaymentOptions
 } from "@shopify/ui-extensions-react/checkout";
-import { useState } from "react";
 
 // 1. Choose an extension target
 export default reactExtension(
@@ -17,52 +16,17 @@ export default reactExtension(
 function Extension() {
   const options = useSelectedPaymentOptions();
   const { shop, checkoutToken } = useApi();
-  const [isInvoiceSettled, setIsInvoiceSettled] = useState(false);
-  const [isCheckingPayment, setIsCheckingPayment] = useState(false);
   const hasManualPayment = options.some((option) => option.type.toLowerCase() === 'manualpayment');
-
   const appUrl = `PLUGIN_URL/checkout?checkout_token=${checkoutToken.current}`;
-  const validatePaymentUrl = `PLUGIN_URL/validate-payment?checkout_token=${checkoutToken.current}`;
-
-  const checkPaymentStatus = async () => {
-    try {
-      const response = await fetch(validatePaymentUrl);
-      if (!response.ok) return;
-
-      const data = await response.json();
-      if (data.IsInvoiceSettled) {
-        setIsInvoiceSettled(true);
-        setIsCheckingPayment(false);
-      }
-    } catch (error) {}
-  };
-
-
-  const startCheckingPayment = () => {
-    if (isCheckingPayment) return;
-
-    setIsCheckingPayment(true);
-    const interval = setInterval(async () => {
-      await checkPaymentStatus();
-    }, 4000);
-    return () => clearInterval(interval);
-  };
-
 
   if (!hasManualPayment) return null;
 
   return (
     <BlockStack>
       <Text>Shop name: {shop.name}</Text>
-      {isInvoiceSettled ? (
-        <Text size="large" alignment="center" bold>Payment received. Thank you for your order!</Text>
-      ) : (
-        <>
-          <Text size="large" alignment="center" bold>Review and pay using BTCPay Server!</Text>
-          <Text>Please review your order and complete the payment using BTCPay Server.</Text>
-          <Button to={appUrl} external onPress={startCheckingPayment}>Complete Payment</Button>
-        </>
-      )}
+      <Text size="large" alignment="center" bold>Review and pay using BTCPay Server!</Text>
+      <Text>Please review your order and complete the payment using BTCPay Server.</Text>
+      <Button to={appUrl} external>Complete Payment</Button>
     </BlockStack>
   );
 }
